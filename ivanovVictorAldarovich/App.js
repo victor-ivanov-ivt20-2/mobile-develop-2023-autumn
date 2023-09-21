5246f40844758c5f3cd68d402e38be0a3507efdd
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-09-21 19:33:29 +0900

    add button count

--- ivanovVictorAldarovich/App.js
+++ ivanovVictorAldarovich/App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  const addCount = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const decreseCount = () => {
+    setCount((prev) => prev - 1);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>100 лет страданий</Text>
+      <TouchableOpacity style={styles.button} onPress={addCount}>
+        <Text>Увеличь страдания</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={decreseCount}>
+        <Text>Пощадить меня</Text>
+      </TouchableOpacity>
+      <Text>{count} лет страданий</Text>
       <Text>Иванов Виктор ИВТ-20-2</Text>
       <StatusBar style="auto" />
     </View>
@@ -17,5 +34,10 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    padding: 10,
+    borderRadius: 5,
   },
 });
