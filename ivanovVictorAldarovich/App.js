81ea3bc1cd0dcd9f9219def8bef583c1d1011c18
Victor <72002526+victor-ivanov-ivt20-2@users.noreply.github.com>
2023-09-21 20:13:13 +0900

    add icons to nav

--- ivanovVictorAldarovich/App.js
+++ ivanovVictorAldarovich/App.js
@@ -7,12 +7,32 @@ import Lab2 from "./screens/Lab2";
 
 import { NavigationContainer } from "@react-navigation/native";
 
+import Ionicons from "react-native-vector-icons/Ionicons";
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Лабораторная 1") {
+              iconName = focused
+                ? "ios-information-circle"
+                : "ios-information-circle-outline";
+            } else if (route.name === "Лабораторная 2") {
+              iconName = focused ? "ios-list" : "ios-list-outline";
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
         <Tab.Screen name="Лабораторная 1" component={Lab1} />
         <Tab.Screen name="Лабораторная 2" component={Lab2} />
       </Tab.Navigator>
